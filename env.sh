5c227bbfae602c476dda40f07e0a5c46a4bbca31
perugi <dominik.perusko@gmail.com>
2023-01-31 17:03:26 +0100

    Move existing flask implementation into a subfolder

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export DATABASE_URL="postgres://xszrtixzwpmvvc:7de1ca1b88b8058eaffd071bfb6dca12346389fc655a914fb823bedb921e885c@ec2-52-208-164-5.eu-west-1.compute.amazonaws.com:5432/d8gheu64ohd5vq"
+export FLASK_DEBUG=1
+export MAIL_USERNAME="plannter.web@gmail.com"
+export MAIL_PASSWORD="mpqxnrfmyugukzvh"
+
+echo "Environment variables set!"
