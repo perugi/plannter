180d79d64ca8b16291d1ac2d4b64378a6ed32152
perugi <dominik.perusko@gmail.com>
2023-02-07 10:44:07 +0100

    Fix todos field of the model

--- garden_calendar/models.py
+++ garden_calendar/models.py
@@ -18,7 +18,10 @@ class Plant(models.Model):
     creator = models.ForeignKey("User", on_delete=models.CASCADE)
     name_en = models.CharField(max_length=64)
     name_si = models.CharField(max_length=64)
-    todos = models.CharField
+    todos = models.CharField(max_length=110, default="")
+
+    def __str__(self):
+        return f"{self.name_si}: [{self.creator}]"
 
 
 class SelectedPlant(models.Model):
