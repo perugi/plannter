a0cb2c3503784a616ce29a48b95f74eb41001780
perugi <dominik.perusko@gmail.com>
2023-02-07 08:13:40 +0100

    Add models to admin view

--- garden_calendar/admin.py
+++ garden_calendar/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Settings, Plants, SelectedPlants
+
+admin.site.register(User)
+admin.site.register(Settings)
+admin.site.register(Plants)
+admin.site.register(SelectedPlants)
